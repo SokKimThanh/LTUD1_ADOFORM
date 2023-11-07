f665e7f11ee2bdc817c860a770585ba7c7143401
Sok Kim Thanh <thanhsk1991@gmail.com>
2023-11-07 23:22:59 +0700

    add files code mẫu thao tác trên cơ sở dữ liệu chiến thuật đóng mở kết nối cơ sở dữ liệu

---
+++ connection/DBController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADOForm.Conection
+{
+    /// <summary>
+    /// Lớp DBController là lớp định nghĩa các thao tác trên cơ sở dữ liệu
+    /// 11/07/2023
+    /// </summary>
+    internal interface DBController
+    {
+        /// <summary>
+        /// Thao tác thêm
+        /// </summary>
+        /// <param name="sender">Tên lớp</param>
+        public void Insert(object sender);
+        /// <summary>
+        /// Thao tác sửa
+        /// </summary>
+        /// <param name="sender">Tên lớp</param>
+        public void Update(object sender);
+        /// <summary>
+        /// Thao tác xóa
+        /// </summary>
+        /// <param name="id">mã</param>
+        public void Delete(object id);
+        /// <summary>
+        /// Thao tác liệt kê
+        /// </summary> 
+        public void SelectAll();
+        /// <summary>
+        /// Thao tác tìm kiếm
+        /// </summary>
+        /// <param name="id">Mã</param>
+        /// <returns>Trả về một đối tượng của một lớp</returns>
+        public DataTable SelectByID(object id);
+    }
+}
---
+++ controller/UserController.cs
@@ -0,0 +1,138 @@
+﻿using ADOForm.Conection;
+using ADOForm.model;
+using Microsoft.VisualBasic.ApplicationServices;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace ADOForm.controller
+{
+
+    public class UserController : MyController
+    {
+        public UserController(string connectionString) : base(connectionString)
+        {
+
+        }
+
+        public override void Delete(object id)
+        {
+            // Mở kết nối
+            SqlConnection conn = OpenConnection();
+
+            // Tạo một đối tượng SqlCommand
+            Sql = new SqlCommand("YourDeleteStoredProcedureName", conn);
+            Sql.CommandType = CommandType.StoredProcedure;
+
+            // Thêm tham số vào SqlCommand
+            Sql.Parameters.AddWithValue("@UserId", id);
+
+            // Thực thi SqlCommand
+            Sql.ExecuteNonQuery();
+
+            // Đóng kết nối
+            CloseConnection();
+        }
+
+        public override void Insert(object sender)
+        {
+            User user = (User)sender;
+            // Mở kết nối
+            SqlConnection conn = OpenConnection();
+
+            // Tạo một đối tượng SqlCommand
+            Sql = new SqlCommand("YourInsertStoredProcedureName", conn);
+            Sql.CommandType = CommandType.StoredProcedure;
+
+            // Thêm tham số vào SqlCommand
+            Sql.Parameters.AddWithValue("@Username", user.Username);
+            Sql.Parameters.AddWithValue("@Password", user.Password);
+            Sql.Parameters.AddWithValue("@CreatedDate", user.CreatedDate);
+
+            // Thực thi SqlCommand
+            Sql.ExecuteNonQuery();
+
+            // Đóng kết nối
+            CloseConnection();
+        }
+
+        public override void SelectAll()
+        {
+            // Mở kết nối
+            SqlConnection conn = OpenConnection();
+
+            // Tạo một đối tượng SqlCommand
+            Sql = new SqlCommand("YourStoredProcedureName", conn);
+            Sql.CommandType = CommandType.StoredProcedure;
+
+            // Tạo một đối tượng SqlDataAdapter
+            Adapter = new SqlDataAdapter(Sql);
+
+            // Tạo một đối tượng DataTable để lưu trữ dữ liệu
+            Listgridview = new DataTable();
+
+            // Đổ dữ liệu vào DataTable
+            Adapter.Fill(Listgridview);
+
+            // Đóng kết nối
+            CloseConnection();
+        }
+
+        public override DataTable SelectByID(object id)
+        {
+            // Mở kết nối
+            SqlConnection conn = OpenConnection();
+
+            // Tạo một đối tượng SqlCommand
+            Sql = new SqlCommand("YourSelectByIdStoredProcedureName", conn);
+            Sql.CommandType = CommandType.StoredProcedure;
+
+            // Thêm tham số vào SqlCommand
+            Sql.Parameters.AddWithValue("@UserId", id);
+
+            // Tạo một đối tượng SqlDataAdapter
+            Adapter = new SqlDataAdapter(Sql);
+
+            // Tạo một đối tượng DataTable để lưu trữ dữ liệu
+            Listgridview = new DataTable();
+
+            // Đổ dữ liệu vào DataTable
+            Adapter.Fill(Listgridview);
+
+            // Đóng kết nối
+            CloseConnection();
+
+            // Trả về DataTable
+            return Listgridview;
+        }
+
+        public override void Update(object sender)
+        {
+            User user = (User)sender;
+            // Mở kết nối
+            SqlConnection conn = OpenConnection();
+
+            // Tạo một đối tượng SqlCommand
+            Sql = new SqlCommand("YourUpdateStoredProcedureName", conn);
+            Sql.CommandType = CommandType.StoredProcedure;
+
+            // Thêm tham số vào SqlCommand
+            Sql.Parameters.AddWithValue("@UserId", user.Userid);
+            Sql.Parameters.AddWithValue("@Username", user.Username);
+            Sql.Parameters.AddWithValue("@Password", user.Password);
+            Sql.Parameters.AddWithValue("@CreatedDate", user.CreatedDate);
+
+            // Thực thi SqlCommand
+            Sql.ExecuteNonQuery();
+
+            // Đóng kết nối
+            CloseConnection();
+        }
+    }
+
+}
